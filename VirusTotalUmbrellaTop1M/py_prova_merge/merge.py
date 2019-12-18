a486ed463e0b81279eea3ce0fcaa253185aaef9f
GiuseppePercuoco94 <giuseppepercuoco94@gmail.com>
2019-12-18 21:54:49 +0100

    aggiunto prova merge

--- VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
+++ VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
@@ -42,8 +42,11 @@ for each file in "csv_204_error_folder"
 
 import os
 import shutil
+from jsonmerge import merge
+import json
+import glob
 
-path = "/Volumes/PEPPE_DT/WorkspacePyCharm/VirusTotalUmbrellaTop1M/py_prova_merge"
+path = "/Volumes/PEPPE_DT/WorkspacePyCharm/VirusTotalUmbrellaTop1M/py_prova_merge/prova_merge"
 path = os.path.relpath(path)
 # print(path)
 
@@ -67,16 +70,61 @@ for file in os.listdir(path):
         shutil.move(path + "/" + file, csv_204_error_folder)
 
 try:
-    all_filter = open(path + "/all_filter.json", "+a")
+    all_filter = open(path + "/all_filter.json", "+w")
 except IOError:
     print("Filed to open the 'all_filter' file")
 
+result_filter = []
+for f in glob.glob(csv_filter_folder + "/*.json"):
+    with open(f, "r") as infile:
+        result_filter.append(json.load(infile))
+with open(path + "/all_filter.json", "+w") as outfile:
+    json.dump(result_filter, outfile, indent=2)
 
+result_filter_204 = []
+for f in glob.glob(csv_204_error_folder + "/*.json"):
+    with open(f, "r") as infile:
+        result_filter_204.append(json.load(infile))
+with open(path + "/all_200_error.json") as outfile:
+    json.dump(result_filter_204, outfile, indent=2)
+
+
+
+'''
+for file in os.listdir(csv_filter_folder):
+    file_open = open(file, "wb+")
+    lines = file_open.readlines()
+    for line in lines:
+        print(line)
+'''
+
+'''
+for file in os.listdir(csv_filter_folder):
+    infilename = os.path.join(csv_filter_folder, file)
+    oldbase = os.path.splitext(file)
+    newname = infilename.replace('.json', '.txt')
+    os.rename(infilename, newname)
+   # print("infilename:" + infilename + " ,newname:" + newname)
+    file_txt = open(newname, "r")
+    lung = len(file_txt.readlines())
+    file_line = file_txt.readline()
+    print(lung)
+    print(" tipo di file_line: " + str(type(file_line)))
+    print("primo elemento:" + str(file_line[0]))
+    for i in range(1, lung):
+        print(file_line[i])
+        all_filter.write(file_line[i])
+
+   # all_filter = merge(all_filter, file)
+'''
+all_filter.close()
+
+'''
 all_filter.write("{\n")
 
 for file in os.listdir(csv_filter_folder):
-    print("File: "+ file + ": opened \n")
-    file_read = open(file, "w+")
+    print("File: " + file + ": opened \n")
+    file_read = open(file)
     file_len = len(file_read.readlines())
     print(file_len)
     for lin in range(1, file_len - 1):
@@ -84,7 +132,7 @@ for file in os.listdir(csv_filter_folder):
     print(file + " add to 'all_filter'.\n")
 all_filter.write("}")
 all_filter.close()
-
+'''
 '''
 for file in csv_filter_folder:
         print ("File:" + file + " opened \n")
