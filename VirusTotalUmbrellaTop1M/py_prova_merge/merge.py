376effcdf4e39b632c2f895f56602dcf0b559dbf
GiuseppePercuoco94 <giuseppepercuoco94@gmail.com>
2020-01-07 12:10:58 +0100

    name modify of py file to 'tranco_prova', add 'tranco prova' to fp_vt of 'merge-py'

--- VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
+++ VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
@@ -3,6 +3,9 @@ import shutil
 import json
 import glob
 from jsonmerge import merge
+import csv
+import time
+from datetime import  datetime
 
 '''
                                     #######"filter_merge"#######
@@ -23,11 +26,6 @@ write a new file "all_filter.json" with all csv "filter"
 write a new file "all_204_error.json" with all csv "204_error"
 
 '''
-
-
-
-
-
 def filter_merge(csv_filter_fold, csv_204_error_fold, string_path_json_filter, string_path_204):
 
     """
@@ -115,15 +113,63 @@ Aim: calculate the FP rate caused by VirusTotal
 '''
 
 
-def fp_vt(csv_tranco, json_all_filter_in):
+def fp_vt(tranco_csv, all_filter_json):
 
     """
-    :param csv_tranco: csv of Tranco rank
-    :param json_all_filter_in: path of 'all_filter.json'
+    :param tranco_csv: csv of Tranco rank
+    :param all_filter_json: path of 'all_filter.json'
     :return:
     """
-    exit()
+    with open(all_filter_json) as file_json:
+        json_data = json.load(file_json)
 
+    '''
+    list of domain name scanned (key values of dict 'json_data')
+    '''
+    index = []
+    for a in json_data.items():
+        index.append(a[0])
+
+    '''
+    keep second column of tranco csv
+    '''
+    delimiter = ','
+    tranco_domain = []
+    with open(tranco_csv, 'r') as tranco:
+        reader = csv.reader(tranco, delimiter=delimiter)
+        for row in reader:
+            tranco_domain.append(row[1])
+
+    '''
+     - check the presence of domain name in tranco csv with a score number > 0, and add them to a new dict.
+     - calulate the time to analyse 
+    '''
+    start = datetime.now()
+    fp_dict = {}
+    score_l = []
+    for x in index:
+        for p in json_data[x]:
+            if p['score'] > 0:
+                print(x)
+                for dom in tranco_domain:
+                    if dom == x:
+                        fp_dict[x] = p['score']
+                        score_l.append(p['score'])
+    end = datetime.now()
+    for x, y in fp_dict.items():
+        print(x, y)
+
+    print('Duration: {}'.format(end - start))
+    print(len(index))
+    print(len(fp_dict))
+    percent = (len(fp_dict) / len(index))
+    print("% :" + str(percent))
+    maximum = max(score_l)
+    print("Max score: " + str(maximum))
+
+    for x in range(1, maximum + 1):
+        eq = sum(num == x for num in score_l)
+        print("Num of score equal to " + str(x) + " is :" + str(eq))
 
 if __name__ == "__main__":
 
@@ -152,4 +198,6 @@ if __name__ == "__main__":
     path_csv_filter_all = path + "/all_filter.json"
     path_csv_204_error = path + "/all_204_error.json"
     filter_merge(csv_filter_folder, csv_204_error_folder, path_csv_filter_all, path_csv_204_error)
+    tranco_path = "/Volumes/PEPPE_DT/WorkspacePyCharm/VirusTotalUmbrellaTop1M/py_prova_merge/tranco-top-1m.csv"
+    fp_vt(tranco_path, path_csv_filter_all)
 
