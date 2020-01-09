1649b115bfaec66c8b74c46709e29ec9809ccffe
GiuseppePercuoco94 <giuseppepercuoco94@gmail.com>
2020-01-09 11:32:58 +0100

    add plotting, add function in merge to save in csv different lenght of json, add img bar plot for 1m,500k,100k

--- VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
+++ VirusTotalUmbrellaTop1M/py_prova_merge/merge.py
@@ -5,7 +5,9 @@ import glob
 from jsonmerge import merge
 import csv
 import time
-from datetime import  datetime
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
 '''
                                     #######"filter_merge"#######
@@ -26,8 +28,9 @@ write a new file "all_filter.json" with all csv "filter"
 write a new file "all_204_error.json" with all csv "204_error"
 
 '''
-def filter_merge(csv_filter_fold, csv_204_error_fold, string_path_json_filter, string_path_204):
 
+
+def filter_merge(csv_filter_fold, csv_204_error_fold, string_path_json_filter, string_path_204):
     """
     :param csv_filter_fold: folder with only json ~ "csv_filter"
     :param csv_204_error_fold: folder with only json ~ "csv_204_error"
@@ -55,7 +58,7 @@ def filter_merge(csv_filter_fold, csv_204_error_fold, string_path_json_filter, s
     with open(string_path_json_filter, "+w") as outfile:
         json.dump(result_filter, outfile, indent=2)
 
-   # result_filter_204 = {}
+    # result_filter_204 = {}
 
     '''
     for f in glob.glob(csv_204_error_fold + "/*.json"):
@@ -75,11 +78,6 @@ def filter_merge(csv_filter_fold, csv_204_error_fold, string_path_json_filter, s
         json.dump(result_filter_204, outfile, indent=2)
     
     '''
-
-
-
-
-
 
 
 '''
@@ -119,7 +117,6 @@ Aim: calculate the FP rate caused by VirusTotal
 
 
 def fp_vt(tranco_csv, all_filter_json):
-
     """
     :param tranco_csv: csv of Tranco rank
     :param all_filter_json: path of 'all_filter.json'
@@ -135,6 +132,7 @@ def fp_vt(tranco_csv, all_filter_json):
     for a in json_data.items():
         index.append(a[0])
 
+    index_len = len(index)
     '''
     keep second column of tranco csv
     '''
@@ -152,12 +150,16 @@ def fp_vt(tranco_csv, all_filter_json):
     start = datetime.now()
     fp_dict = {}
     score_l = []
+    all_score_not_null = {}
     for x in index:
         for p in json_data[x]:
             if p['score'] > 0:
+                # dict : domain with score not null
+                all_score_not_null[x] = p['score']
                 print(x)
                 for dom in tranco_domain:
                     if dom == x:
+                        # dict : domain with score not null and in tranco csv
                         fp_dict[x] = p['score']
                         score_l.append(p['score'])
     end = datetime.now()
@@ -168,18 +170,90 @@ def fp_vt(tranco_csv, all_filter_json):
     print(len(index))
     print(len(fp_dict))
     percent = (len(fp_dict) / len(index))
-    print("% :" + str(percent*100))
+    print("% :" + str(percent * 100))
     maximum = max(score_l)
     print("Max score: " + str(maximum))
 
+    if index_len <= 100000:
+        try:
+            with open("100K_vt.csv", "w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key,value in fp_dict.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error vt write csv out 100")
+        try:
+            with open("100K_full.csv","w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key, value in all_score_not_null.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error fill write csv out 100")
+    elif 100000 < index_len <= 500000:
+        try:
+            with open("500K_vt.csv", "w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key, value in fp_dict.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error vt write csv out 500")
+        try:
+            with open("500K_full.csv","w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key,value in all_score_not_null.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error fill write csv out 500")
+    else:
+        try:
+            with open("1m_vt.csv", "w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key, value in fp_dict.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error vt write csv out 1m")
+        try:
+            with open("1m_full.csv","w", newline="") as out:
+                writer_vt = csv.writer(out)
+                for key, value in all_score_not_null.items():
+                    writer_vt.writerow([key,value])
+        except IOError:
+            print("I/O error fill write csv out 1m")
+    '''
+    calculate number of occurrences per score from dict obtained by check with trunco
+    '''
+    data_plot = {}
     for x in range(1, maximum + 1):
         eq = sum(num == x for num in score_l)
         print("Num of score equal to " + str(x) + " is :" + str(eq))
+        data_plot[str(x)] = eq
+
+    '''
+    fig_100K, ax1 = plt.subplots()
+    ax1.set_title('Basic Plot')
+    ax1.boxplot(score_l)
+    plt.show()
+    '''
+    '''
+    names = list(data_plot.keys())
+    values = list(data_plot.values())
+    fig, ax = plt.subplots()
+    ax.bar(names, values)
+
+    if index_len <= 100000:
+        fig.suptitle("100K bar plot")
+    elif 100000 < index_len <= 500000:
+        fig.suptitle("500k bar plot")
+    else:
+        fig.suptitle("1m bar plot")
+
+    plt.show()
+    '''
 
 
 if __name__ == "__main__":
-
     path = "/Volumes/PEPPE_DT/WorkspacePyCharm/VirusTotalUmbrellaTop1M/py_prova_merge/prova_merge"
+
     path = os.path.relpath(path)
     # print(path)
 
@@ -206,4 +280,3 @@ if __name__ == "__main__":
     filter_merge(csv_filter_folder, csv_204_error_folder, path_csv_filter_all, path_csv_204_error)
     tranco_path = "/Volumes/PEPPE_DT/WorkspacePyCharm/VirusTotalUmbrellaTop1M/py_prova_merge/tranco-top-1m.csv"
     fp_vt(tranco_path, path_csv_filter_all)
-
